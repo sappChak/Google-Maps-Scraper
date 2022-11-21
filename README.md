9c04c934c9436f170f92b1b3ba8865448d6f40c2
Andrew <38425829+sappChak@users.noreply.github.com>
2022-11-21 15:05:32 +0100

    Just to ensure that I have not forgotten something

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GoogleMapsScraper
+![img.png](img.png)
+![img_1.png](img_1.png)
+
+Coordinates have to look this way:
+40.730610,-73.935242
--- places.py
+++ places.py
@@ -3,9 +3,10 @@ from tabulate import tabulate
 
 
 def get_places_table(place_coordinates: str, place_radius, keyword: str, limit: int):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={place_coordinates}&radius={place_radius}&keyword={keyword}&key=YOUR_KEY_HERE"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=nextPageToken&location={place_coordinates}&radius={place_radius}&keyword={keyword}&key=AIzaSyAuFlBfY0G-N7voGx7ckRwlWuGWShRyDz4"
 
     response = requests.request("GET", url).json()
+    print(response)
 
     table = []
     for index, item in enumerate(response['results']):
